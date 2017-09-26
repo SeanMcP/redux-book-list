310cfae4ba91821bab957abf3c331c16cfe2beab
Sean McPherson <seanmcp25@gmail.com>
2017-09-26 14:06:58 -0400

    Map over book data on BookList container

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -8,29 +8,23 @@ import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   render() {
-
     //must create a map function here to return the following:
-
-
-    // ******************
-    //
-    //     <li
-    //       key={book.id}
-    //       onClick={() => this.props.selectBook(book)}
-    //       className="list-group-item">{book.title}</li>
-    //
-    // ******************
+    let booksMap = this.props.books.map(book => {
+      return(
+        <li key={book.id} onClick={() => this.props.selectBook(book)} className="list-group-item">{book.title}</li>
+      )
+    })
 
     return (
       <ul className="list-group col-sm-4">
-        {/* return your mapped array list items here */}
+        {booksMap}
       </ul>
     );
   }
 }
 
 function mapStateToProps(state) {
-  console.log("mapstate", state.books);
+  // console.log("mapstate", state.books);
   //what is returned will show up as props inside of BookList
   //this gives you access to books in props.. (books would be good for mapping)
   return {
