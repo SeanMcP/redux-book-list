0514db3e80e85dd6583c64727aa9f48da4ccd645
Sean McPherson <seanmcp25@gmail.com>
2017-09-26 13:45:59 -0400

    Update ReactDOM to refer to #root

--- src/index.js
+++ src/index.js
@@ -3,21 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 
-
-
 //redux imports
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import promise from 'react-redux';
 //import promise from 'redux-promise';
 
 //react router imports
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
 //components
 import App from './components/App';
-
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -27,4 +24,4 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('#root'));
