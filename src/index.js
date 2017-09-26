0461078e29cb44f8b9ae1c3ca8bdf67bac062a05
Sean McPherson <seanmcp25@gmail.com>
2017-09-26 13:41:55 -0400

    Add starter files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+//react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+//redux imports
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+//import promise from 'redux-promise';
+
+//react router imports
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+//components
+import App from './components/App';
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+//switch uses most specific route that matches, top down.
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
